7956b23df35f091a59eb605a93fe7a4c86c767d3
stan1812 <2314980309@qq.com>
2018-05-09 00:22:05 +0800

    use mysql

--- Models/Team.java
+++ Models/Team.java
@@ -1,11 +1,55 @@
 package Models;
+import java.sql.*;
 
 public class Team {
     public static void getTeam() {
 
     }
 
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://119.29.235.55:3306/javaexp";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "javaexp";
+    static final String PASS = "gYEcAeFHOVMz8xPY";
+
     public static void main(String[] args) {
-        getTeam();
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SHOW TABLES";
+            ResultSet rs = stmt.executeQuery(sql);
+            // 展开结果集数据库
+            while(rs.next()){
+
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
     }
 }
