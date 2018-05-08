b21d5afe87889caa598117b4cd6c3f3cf8a8c62a
stan1812 <2314980309@qq.com>
2018-05-09 01:09:29 +0800

    test mysql CURD

--- Models/Team.java
+++ Models/Team.java
@@ -1,10 +1,12 @@
 package Models;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Team {
-    public static void getTeam() {
 
-    }
 
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://119.29.235.55:3306/javaexp";
@@ -13,43 +15,45 @@ public class Team {
     static final String USER = "javaexp";
     static final String PASS = "gYEcAeFHOVMz8xPY";
 
-    public static void main(String[] args) {
+    public static void operateData(String sql) {
         Connection conn = null;
         Statement stmt = null;
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("连接数据库...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println(" 实例化Statement对象...");
             stmt = conn.createStatement();
-            String sql;
-            sql = "SHOW TABLES";
-            ResultSet rs = stmt.executeQuery(sql);
-            // 展开结果集数据库
-            while(rs.next()){
-
-            }
-            rs.close();
+            int rows = stmt.executeUpdate(sql);
+            System.out.println(rows);
             stmt.close();
             conn.close();
-        }catch(SQLException se){
+        } catch (SQLException se) {
             // 处理 JDBC 错误
             se.printStackTrace();
-        }catch(Exception e){
+        } catch (Exception e) {
             // 处理 Class.forName 错误
             e.printStackTrace();
-        }finally{
+        } finally {
             // 关闭资源
-            try{
-                if(stmt!=null) stmt.close();
-            }catch(SQLException se2){
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
             }
-            try{
-                if(conn!=null) conn.close();
-            }catch(SQLException se){
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
                 se.printStackTrace();
             }
         }
-        System.out.println("Goodbye!");
+    }
+
+    public static void getTeam() {
+
+    }
+//待添加不同sql处理.睡了..
+    public static void main(String[] args) {
+        operateData("INSERT INTO player VALUES (2,'f','f','fff',10)");
+
     }
 }
